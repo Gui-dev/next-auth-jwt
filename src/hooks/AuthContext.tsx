9a2369b06c770230c7d9a7e9f2948617daf3045d
Gui-dev <adrianon10@gmail.com>
2021-06-16 03:39:00 -0300

    feat: add context AuthContext

---
+++ src/hooks/AuthContext.tsx
@@ -0,0 +1,65 @@
+import React, { createContext, useContext, useEffect, useState } from 'react'
+import { setCookie, parseCookies } from 'nookies'
+import Router from 'next/router'
+
+import { recoverUserInformation, signInRequest } from '../services/auth'
+
+interface AuthContextProps {
+  isAuthenticated: boolean
+  user: IUserProps | null
+  signIn: (data: ISignInData) => void
+}
+
+interface ISignInData {
+  email: string
+  password: string
+}
+
+interface IUserProps {
+  name: string
+  email: string
+  avatar_url: string
+}
+
+const AuthContext = createContext({} as AuthContextProps)
+
+export const AuthProvider: React.FC = ({ children }) => {
+  const [user, setUser] = useState<IUserProps | null>(null)
+  const isAuthenticated = !!user
+
+  useEffect(() => {
+    const { '@nextauth:token': token } = parseCookies()
+
+    if (token) {
+      recoverUserInformation()
+        .then(response => setUser(response.user))
+    }
+  }, [])
+
+  const signIn = async ({ email, password }: ISignInData) => {
+    const { user, token } = await signInRequest({ email, password })
+
+    setCookie(undefined, '@nextauth:token', token, {
+      maxAge: 60 * 60 * 1 // 1 hour
+    })
+    setUser(user)
+
+    Router.push('/dashboard')
+  }
+
+  return (
+    <AuthContext.Provider value={{
+      isAuthenticated,
+      user,
+      signIn
+    }}>
+      { children }
+    </AuthContext.Provider>
+  )
+}
+
+export const useAuth = () => {
+  const context = useContext(AuthContext)
+
+  return context
+}
